d0da120827748b28bc9b0b56a960323cdd620dab
deastin-teach <deastin@dallasisd.org>
2026-06-10 19:21:32 +0000

    Starting app development

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+from pathlib import Path
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+#Page settings
+st.set_page_config(
+    page_title="School Events App",
+    page_icon="📅",
+    layout="wide"
 )
+
+# Logo
+logo_path = Path(__file__).parent / "logo.png"
+st.image(str(logo_path), width=200)
+
+#Title
+st.title("Welcome to the School Events App")
+
+#Description
+st.write("""
+         This application allows parents, students, teachers,
+         and staff members to view upcoming school events
+         and register for events happening on campus.
+         
+         Select your role below to get started.
+         """)
